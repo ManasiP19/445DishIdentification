14042b179457c2ab25d3f1500bcd94a4c9640f7a
LauraW0622 <ljw5533@psu.edu>
2023-11-21 08:12:30 -0500

    model, form, db

--- DishID/DishID/settings.py
+++ DishID/DishID/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,6 +52,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'DishID.urls'
+
+# Base URL to serve media files
+MEDIA_URL ='/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 TEMPLATES = [
     {
--- DishID/dishIDapp/views.py
+++ DishID/dishIDapp/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 from django.template import loader
+from .forms import ImageForm
+import requests
 
 
 # Create your views here.
 def mainPage(request):
-    template = loader.get_template('mainPage.html')
-    return HttpResponse(template.render())
+   # Process images uploaded
+   if request.method == 'POST':
+      form = ImageForm(request.POST, request.FILES)
+      if form.is_valid(): 
+         form.save()
+
+         # Get current instance obj to display in template
+         img_obj = form.instance  #send this to model
+         foodName = img_obj.title
+
+         query = foodName 
+         api_url = 'https://api.api-ninjas.com/v1/recipe?query={}'.format(query) 
+         response = requests.get(api_url, headers={'X-Api-Key': '4AcO9BzqnfUen6lkgeBLag==pu3HsjPTjgk4VXOW'}) 
+         if response.status_code == requests.codes.ok:
+            apiResponse = response.json()      
+         return render(request, 'mainPage.html', {'form': form, 'img_obj': img_obj, 'apiResponse': apiResponse})
+   else: 
+      form = ImageForm()
+      return render(request, 'mainPage.html', {'form': form})
+   
