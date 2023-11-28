b9b4ebd47c1bc7500a159edb8c316e70c4b8f161
LauraW0622 <ljw5533@psu.edu>
2023-11-28 09:34:00 -0500

    model

--- DishID/dishIDapp/views.py
+++ DishID/dishIDapp/views.py
@@ -6,7 +6,7 @@ import requests
 from tensorflow.keras.models import load_model
 
 # Load the saved model
-saved_model_path = '../../saved_model.h5'
+saved_model_path = 'C:\\Users\\mrswi\\OneDrive\\Documents\\Penn State\\Machine Learning\\Final.FinalProject\\445DishIdentification\\DishID\\dishIDapp\\savedModels\\googlenet.keras'
 loaded_model = load_model(saved_model_path)
 
 # Create your views here.
@@ -18,10 +18,12 @@ def mainPage(request):
             form.save()
 
             # Get current instance obj to display in template
-            img_obj = form.instance  #send this to model
-            foodName = img_obj.title
+            img_obj = form.instance  
+            imageForModel = img_obj.image
 
-            query = foodName 
+            prediction = load_model.predict(imageForModel)
+
+            query = prediction 
             api_url = 'https://api.api-ninjas.com/v1/recipe?query={}'.format(query) 
             response = requests.get(api_url, headers={'X-Api-Key': '4AcO9BzqnfUen6lkgeBLag==pu3HsjPTjgk4VXOW'}) 
             if response.status_code == requests.codes.ok:
