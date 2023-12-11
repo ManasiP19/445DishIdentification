26d43a83f189c98c2a2e8f3a5055474c4e51618e
Tanvi Pabbati <tmp5876@psu.edu>
2023-12-10 19:19:35 -0500

    googlenet graph

--- GoogLeNet.py
+++ GoogLeNet.py
@@ -1,19 +1,24 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.base import accuracy_score
+from sklearn.metrics import classification_report
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import img_to_array, array_to_img
+from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, load_model
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from PIL import Image
-from load_images import load_images
+from load_images import load_images_from_zip
 import zipfile
+from image_preprocessing import image_preprocessing
+import os
 
 # Load images and labels
-df = load_images('images')
+df = load_images_from_zip('images.zip')
+df.head()
 
 # Encode labels
 label_encoder = LabelEncoder()
@@ -23,26 +28,40 @@ num_classes = len(label_encoder.classes_)
 # Split the dataset into training and testing sets
 train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
-def custom_data_generator(dataframe, batch_size, img_size):
+def custom_data_generator(dataframe, batch_size, img_size, preprocessing_params=None):
     while True:
         for i in range(0, len(dataframe), batch_size):
-            batch_df = dataframe.iloc[i:i+batch_size]
+            batch_df = dataframe.iloc[i:i + batch_size]
             batch_images = []
             batch_labels = []
-            for _, row in batch_df.iterrows():
-                img = Image.open(row['image_path'])
-                img = img.resize(img_size)
-                img_array = img_to_array(img)
-                img_array /= 255.0
-                batch_images.append(img_array)
-                batch_labels.append(label_encoder.transform([row['labels']])[0])
+            for index, row in batch_df.iterrows():
+                with zipfile.ZipFile('images.zip', 'r') as zip_ref:
+                    with zip_ref.open(row['image_path']) as file:
+                        img = Image.open(file)
+                        img = img.resize(img_size)
+                        img_array = image_preprocessing(img, **preprocessing_params)
+                        img_array = img_to_array(img_array)
+                        img_array /= 255.0
+                        batch_images.append(img_array)
+                        batch_labels.append(label_encoder.transform([row['labels']])[0])
             batch_labels = tf.keras.utils.to_categorical(batch_labels, num_classes=num_classes)
             yield np.array(batch_images), np.array(batch_labels)
 
 img_size = (299, 299)
 batch_size = 32
-epochs = 10
+epochs = 5
 
+# Example usage of custom_data_generator with preprocessing
+preprocessing_params = {
+    'normalize_pixel_vals': True,
+    'flattening': True,
+    'new_dimensions': (100, 100),
+    'sharpening': True,
+    'resize_factor': 0.5,
+    'to_greyscale': True
+}
+
+# Build the model
 base_model = InceptionV3(weights='imagenet', include_top=False)
 
 x = base_model.output
@@ -57,20 +76,51 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 
 # Train the model
 history = model.fit(
-    custom_data_generator(train_df, batch_size, img_size),
+    custom_data_generator(train_df, batch_size, img_size, preprocessing_params=preprocessing_params),
     steps_per_epoch=len(train_df) // batch_size,
     epochs=epochs,
-    validation_data=custom_data_generator(test_df, batch_size, img_size),
+    validation_data=custom_data_generator(test_df, batch_size, img_size, preprocessing_params=preprocessing_params),
     validation_steps=len(test_df) // batch_size
 )
+
+# Plot training history
+def plot_training_history(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 4))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+
+    plt.show()
+
+# Plot the training history
+plot_training_history(history)
 
 # Save the trained model
 model.save('googlenet.h5')
 
+# Load the saved model for testing
+loaded_model = load_model('googlenet.h5')
+
 # Evaluate the model on the test set
-test_generator = custom_data_generator(test_df, batch_size, img_size)
+test_generator = custom_data_generator(test_df, batch_size, img_size, preprocessing_params=preprocessing_params)
 test_steps = len(test_df) // batch_size
-test_predictions = model.predict(test_generator, steps=test_steps)
+test_predictions = loaded_model.predict(test_generator, steps=test_steps)
 
 # Convert predictions to class labels
 predicted_labels = np.argmax(test_predictions, axis=1)
